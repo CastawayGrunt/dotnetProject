b0ad292a9feec0de037424a7ca0f954dec8a58fb
Brendon Woodall <brendonwoodall@Brendons-MacBook-Pro-M3.local>
2024-04-23 08:43:50 -0600

    update order handling Add and Get

--- DotnetProjectApi.API/Features/Orders/Models/OrderModel.cs
+++ DotnetProjectApi.API/Features/Orders/Models/OrderModel.cs
@@ -7,7 +7,7 @@ namespace DotnetProjectApi.API.Features.Orders.Models
         [Key]
         public Guid Id { get; set; }
 
-        public List<OrderDetailModel> Products { get; set; } = new();
+        public List<Guid> Products { get; set; } = new();
         public AddressModel ShippingAddress { get; set; } = new();
         public DateTime? OrderPlacedDateTime { get; set; }
 
@@ -16,6 +16,5 @@ namespace DotnetProjectApi.API.Features.Orders.Models
         public Guid OrderNumber { get; set; }
         // Add when user is implemented
         // public Guid UserId { get; set; }
-
     }
 }
